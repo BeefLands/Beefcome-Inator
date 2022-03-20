3c6617e4aad6c28f99c2fdc0442183d3df1c2f4b
Jaron <jaron_zijlstra@outlook.com>
2022-03-20 23:24:36 +0100

    feat: add beef role on join

--- src/augment.d.ts
+++ src/augment.d.ts
@@ -2,5 +2,6 @@ namespace NodeJS {
 	export interface ProcessEnv {
 		DISCORD_TOKEN: string;
 		WELCOME_CHANNEL: string;
+		BEEF_ROLE: string;
 	}
 }
--- src/listeners/guildMemberAdd.ts
+++ src/listeners/guildMemberAdd.ts
@@ -3,6 +3,7 @@ import { GuildMember, MessageEmbed } from "discord.js";
 
 export class GuildMemberAddListener extends Listener<typeof Events.GuildMemberAdd> {
 	public async run(member: GuildMember) {
+		await member.roles.add(process.env.BEEF_ROLE);
 		const channel = await this.container.client.channels.fetch(process.env.WELCOME_CHANNEL);
 		if (!channel?.isText()) return;
 		await channel.send({
