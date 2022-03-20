770d0e87bfdbb18570745370c47a64604b0f6b10
Jaron <jaron_zijlstra@outlook.com>
2022-03-20 23:19:36 +0100

    feat: add activity

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -4,5 +4,6 @@ import type { Client } from "discord.js";
 export class ReadyListener extends Listener<typeof Events.ClientReady> {
 	public run(client: Client<true>) {
 		this.container.logger.info(`Logged in as ${client.user.tag}!`);
+		this.container.client.user?.setActivity({ type: "WATCHING", name: "people join" });
 	}
 }
