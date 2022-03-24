bf4819805030f710f9484c7f29aec262a050fd8e
Jaron <jaron_zijlstra@outlook.com>
2022-03-24 20:51:14 +0100

    fix: add register path

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import { container, SapphireClient } from "@sapphire/framework";
 import { config } from "dotenv";
 import "@sapphire/plugin-logger/register";
 import { WebhookManagerStore } from "./lib/stores/WebhookManagerStore";
+import { join } from "path";
 config();
 
 const client = new SapphireClient({
 	intents: ["GUILDS", "GUILD_MEMBERS"]
 });
 
-container.stores.register(new WebhookManagerStore());
+container.stores.register(new WebhookManagerStore().registerPath(join(__dirname, "webhooks")));
 
 await client.login();
